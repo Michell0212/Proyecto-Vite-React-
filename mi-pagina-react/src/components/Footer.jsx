6d21530b62e08b547bd5e75ffb85894f3648fbbb
Francis Perez <150402107+13649850@users.noreply.github.com>
2025-12-16 16:33:04 -0500

    3. Footer
    Refactor Footer component to include links to GitHub and LinkedIn.

--- mi-pagina-react/src/components/Footer.jsx
+++ mi-pagina-react/src/components/Footer.jsx
@@ -1,5 +1,28 @@
-function Footer() {
-  return <footer>© 2025 - Proyecto React con Vite</footer>;
-}
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 - Proyecto React | Programación Integrativa | Grupo 5</p>
+
+      <div className="links">
+        <a
+          href="https://github.com/Michell0212/Proyecto-Vite-React-.git"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </div>  
+      < div className="links">
+        <a
+          href="https://www.linkedin.com/in/liliana-elizabeth-lesano-tipantu%EF%BF%BDa-6a9671338/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
