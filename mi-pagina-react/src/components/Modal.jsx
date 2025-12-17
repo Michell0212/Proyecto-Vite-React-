2767f8e89f454a41735870df35cc3d76312c179f
dekoov <dcobel@hotmail.com>
2025-12-17 00:23:41 -0500

    fix: mejora de los datos de tarjetas, y animaciones
    Se modifico features.json para hablar sobre componentes React
    Se agregaron estilos y animaciones a los modals

--- mi-pagina-react/src/components/Modal.jsx
+++ mi-pagina-react/src/components/Modal.jsx
@@ -1,12 +1,22 @@
+import { useEffect } from 'react';
 import '../Modal.css';
 
 function Modal({ data, onClose }) {
+
+    useEffect(() => {
+        const handleEsc = (event) => {
+            if (event.keyCode === 27) onClose();
+        };
+        window.addEventListener('keydown', handleEsc);
+        return () => window.removeEventListener('keydown', handleEsc);
+    }, [onClose]);
+
     if (!data) return null; // Si no hay datos, no renderiza nada
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <button className="close-btn" onClick={onClose}>X</button>
+                <button className="close-btn" onClick={onClose}>&times;</button>
 
                 <h2>{data.title}</h2>
                 {/* Mostramos la descripción corta destacada */}
